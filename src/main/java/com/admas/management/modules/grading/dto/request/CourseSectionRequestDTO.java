eb4924a9999cffd4d7ed580061dd35f6484d435a
tewodros mekonen <braveryboyy@gmail.com>
2026-05-08 14:45:23 -0700

    create section entity and dtos

---
+++ src/main/java/com/admas/management/modules/grading/dto/request/CourseSectionRequestDTO.java
@@ -0,0 +1,4 @@
+package com.admas.management.modules.grading.dto.request;
+
+public class CourseSectionRequestDTO {
+}
---
+++ src/main/java/com/admas/management/modules/grading/model/entity/StudentEnrollment.java
@@ -0,0 +1,4 @@
+package com.admas.management.modules.grading.model.entity;
+
+public class StudentEnrollment {
+}
